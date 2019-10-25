1d20bf3bbeb5ffe4813f00e0248024dbb1879127
ranjen <ranjen@myboost.com.my>
2019-10-25 18:25:39 +0800

    Working with View templates

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -2,13 +2,21 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import services.Counter;
+import views.html.products;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class ApplicationController extends Controller {
+
+    private static final java.util.Map<Integer, String> productMap = new java.util.HashMap<Integer, String>();
+
+    static {
+        productMap.put(1, "Keyboard");
+        productMap.put(2, "Mouse");
+        productMap.put(3, "Monitor");
+    }
 
     @Inject
     public ApplicationController() {}
@@ -23,5 +31,9 @@ public class ApplicationController extends Controller {
 
     public Result helloRedirect() {
         return redirect(controllers.routes.ApplicationController.echo("HelloWorldv2"));
+    }
+
+    public Result listProducts() {
+        return ok(products.render(productMap.values()));
     }
 }
